fd1bf26a3c135c2557cf20e2bbca11f10fda3d81
Zachary Delcoco <zdelcoco@gmail.com>
2023-04-06 11:01:15 -0500

    dynamic inline styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,35 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    };
+    setEnteredValue(event.target.value);    
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    };
     props.onAddGoal(enteredValue);
+    setEnteredValue('');  //reset input field
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input 
+          style={{borderColor: !isValid ? 'red' : 'black', 
+                  background: !isValid ? 'salmon' : 'transparent'}}
+          type="text" 
+          onChange={goalInputChangeHandler} 
+          value={enteredValue ?? ''}/>
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
